6c06915c1df31bc3761e906f51b6a3a500cb6bb3
Manuel Iglesias <manuel.iglesias@plcpower.com>
2018-02-25 17:36:43 -0500

    pushing evry file in my installed system since I have made several inclusions and modifications that I ma not sure how to include in a post-install.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var serveStatic = require('serve-static')
 var app     = express();
 
 app.use(express.static(__dirname + '/node_modules/freeboard'));
-
+app.use(express.static(__dirname + '/node_modules/serve-static'));
 
 var server = app.listen(3000, function() {
 
